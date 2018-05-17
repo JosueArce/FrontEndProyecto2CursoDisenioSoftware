8fd77e48f0f2000d3179e1f9c7059080f6dc1e0d
Unknown <j-david-10@hotmail.com>
2018-05-17 12:34:47 -0600

    Auto stash before merge of "master" and "origin/master"

---
+++ src/app/shared/models/product.model.ts
@@ -0,0 +1,13 @@
+export interface ProductoModel{
+	ID : number;
+	Nombre : string;
+	Descripcion: string;
+	Existencia : number;
+	Precio : number;
+	Categoría : string;
+	DuracionEnvio:number;
+	Estado :  number;
+	Imagen : string;
+	Vendedor : string;
+	TarifaEnvio : number;
+}
