67fc76dbd64884bb4ca4fafb6598a1663a734931
Daniel Montero <d.montero95@gmail.com>
2018-05-19 21:46:46 -0600

    Admin component creado

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -8,8 +8,6 @@ import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { ProductComponent } from './catalog/product/product.component';
 import { PaymentComponent } from './shopping-cart/payment/payment.component';
 import { SellerRegistrationComponent } from './seller-registration/seller-registration.component';
-import { SellersListComponent } from './admin/sellers-list/sellers-list.component';
-import { RequestsComponent } from './admin/requests/requests.component';
 
 const routes : Routes = [
 	{
@@ -41,14 +39,6 @@ const routes : Routes = [
 		component : SellerRegistrationComponent
 	},
 	{
-		path : 'requests',
-		component : RequestsComponent
-	},
-	{
-		path : 'seller-list',
-		component : SellersListComponent
-	},
-	{
 		path : 'cart',
 		component : ShoppingCartComponent
 	},
@@ -69,5 +59,6 @@ export const routingComponents  = [
 		CatalogComponent, 
 		ShoppingCartComponent, 
 		ProductComponent,
-		PaymentComponent
+		PaymentComponent,
+		SellerRegistrationComponent
 	];
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,14 +5,13 @@ import { NgModule } from '@angular/core';
 import { HomeRoutingModule, routingComponents } from './home-routing.module';
 import { CatalogComponent } from './catalog/catalog.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
-import { SellersListComponent } from './admin/sellers-list/sellers-list.component';
-import { RequestsComponent } from './admin/requests/requests.component';
 import { PaymentComponent } from './shopping-cart/payment/payment.component';
 import { ProductComponent } from './catalog/product/product.component';
 import { LoginService } from '../login/login.service';
 import {FormsModule} from "@angular/forms";
 import { FilterDataPipe } from '../shared/pipes/filter-data.pipe';
 import { SellerRegistrationComponent } from './seller-registration/seller-registration.component';
+import { PaginationComponent } from './shared/pagination/pagination.component';
 
 
 
@@ -21,12 +20,11 @@ import { SellerRegistrationComponent } from './seller-registration/seller-regist
     routingComponents,
     CatalogComponent,
     ShoppingCartComponent,
-    SellersListComponent,
-    RequestsComponent,
     PaymentComponent,
     ProductComponent,
     FilterDataPipe,
-    SellerRegistrationComponent
+    SellerRegistrationComponent,
+    PaginationComponent
   ],
   imports: [
     HomeRoutingModule,
