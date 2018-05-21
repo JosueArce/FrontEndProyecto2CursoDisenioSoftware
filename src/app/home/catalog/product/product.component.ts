5303511a508c6e29f33cae0bff963543d8d5fc50
Unknown <j-david-10@hotmail.com>
2018-05-21 13:56:19 -0600

    ya el login funciona en todo lado!

--- src/app/home/catalog/product/product.component.ts
+++ src/app/home/catalog/product/product.component.ts
@@ -5,6 +5,7 @@ import { LoginService } from '../../../login/login.service';
 import { Router } from '@angular/router';
 import { GlobalService } from '../../../shared/handlers/global-service.service';
 import { CartService } from '../../../shared/handlers/cart.handler.service';
+import { LoginModalService } from '../../../login/loginModal.service';
 
 @Component({
   selector: 'app-product',
@@ -14,28 +15,41 @@ import { CartService } from '../../../shared/handlers/cart.handler.service';
 export class ProductComponent implements OnInit {
 
   private selectedProduct : any;
+  private isLogged : boolean = false;
 
   constructor(
   	private productHandler : ProductHandlerService, 
   	private loginService: LoginService,
   	private router: Router,
     private globalHandler : GlobalService,
-    private cartHandler : CartService) { }
+    private cartHandler : CartService,
+    public loginModalService: LoginModalService) { }
 
   ngOnInit() {
   	this.selectedProduct = this.productHandler.getSelectedProduct();
+    this.isLoggedIn();
   }
 
-  addToCart(){
-  	if(!this.globalHandler.isLoggedIn()){
-      this.globalHandler.pushToLocalStorage("currentRoute",this.globalHandler.getCurrentRoute());
-  		this.router.navigate(['login']);
-  	}
-    else{
-      //add product to cart
+  addToCart()
+  {
+    if(this.isLoggedIn){
       this.cartHandler.pushToCartElementList(this.selectedProduct);
       this.router.navigate(['cart']);
     }
+    else this.loginModalService.openDialog();
+  }
+
+  isLoggedIn(){
+    this.globalHandler.userLogged.subscribe({
+      next : (event : any) => {
+        if(!event) {          
+          this.isLogged = false;
+        }
+        else{         
+          this.isLogged = true;
+        }
+      }
+    });
   }
 
 }
